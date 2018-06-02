eebdd31c09a8f14b5c941bafe8735fffb8fe599e
Aaron <aaronrebmann@gmail.com>
2018-06-02 08:25:41 +0200

    Add a basic athena blog

---
+++ config.py
@@ -0,0 +1,7 @@
+config = {
+	"footer"	: "Nothing here",
+	"author"	: "Aaron Meinel",
+	"sidebardesc"	: "What is going on with this sidebar?",
+	"indexdesc"	: "Software Freedom, Science, Mathematics, Cats, Scripting, ...",
+	"title"	: "Aaron Meinel's Blog",
+}
